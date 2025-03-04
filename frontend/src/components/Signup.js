7ff54e5b58e28bbc3871894def4dfe3c5438fc9a
Saad Nadeem <saadnadeem5509@gmail.com>
2025-03-04 11:37:29 +0500

    added actions

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,33 +1,3 @@
-// import React, { useState } from "react";
-// import { signup } from "../api";
-
-// const Signup = () => {
-//   const [user, setUser] = useState({ username: "", email: "", password: "" });
-
-//   const handleChange = (e) => setUser({ ...user, [e.target.name]: e.target.value });
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     try {
-//       await signup(user);
-//       alert("Signup Successful");
-//     } catch (error) {
-//       alert("Signup Failed");
-//     }
-//   };
-
-//   return (
-//     <form onSubmit={handleSubmit}>
-//       <input name="username" placeholder="Username" onChange={handleChange} />
-//       <input name="email" type="email" placeholder="Email" onChange={handleChange} />
-//       <input name="password" type="password" placeholder="Password" onChange={handleChange} />
-//       <button type="submit">Signup</button>
-//     </form>
-//   );
-// };
-
-// export default Signup;
-
 import "../styles.css";
 import React, { useState } from "react";
 import { signup } from "../api";
