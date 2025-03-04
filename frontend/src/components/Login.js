0d4e4476db004a125ee8f3e3ea073d0d45c61653
Saad Nadeem <saadnadeem5509@gmail.com>
2025-03-04 09:28:53 +0500

    v1.2

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { login } from "../api";
+import "../styles.css";
 
 const Login = () => {
   const [user, setUser] = useState({ email: "", password: "" });
@@ -17,12 +18,58 @@ const Login = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input name="email" type="email" placeholder="Email" onChange={handleChange} />
-      <input name="password" type="password" placeholder="Password" onChange={handleChange} />
-      <button type="submit">Login</button>
-    </form>
+    <div className="auth-container">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit} className="auth-form">
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          value={user.email}
+          onChange={handleChange}
+          required
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          value={user.password}
+          onChange={handleChange}
+          required
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
   );
 };
 
 export default Login;
+
+// import React, { useState } from "react";
+// import { login } from "../api";
+
+// const Login = () => {
+//   const [user, setUser] = useState({ email: "", password: "" });
+
+//   const handleChange = (e) => setUser({ ...user, [e.target.name]: e.target.value });
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     try {
+//       const res = await login(user);
+//       alert("Login Successful! Token: " + res.data.token);
+//     } catch (error) {
+//       alert("Login Failed");
+//     }
+//   };
+
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <input name="email" type="email" placeholder="Email" onChange={handleChange} />
+//       <input name="password" type="password" placeholder="Password" onChange={handleChange} />
+//       <button type="submit">Login</button>
+//     </form>
+//   );
+// };
+
+// export default Login;
